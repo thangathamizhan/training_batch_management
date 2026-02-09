e10b2619ab5c86a46e9f2fcf724c5d21675c6c89
Thangathamizhan R <thangathamizhan.r@laderatechnology.com>
2026-02-09 18:01:12 +0530

    attendance

--- model/courseSchema.js
+++ model/courseSchema.js
@@ -1,16 +1,39 @@
 import mongoose from "mongoose";
+ 
+const courseSchema = new mongoose.Schema(
+  {
+    courseName: {
+      type: String,
+      required: true
+    },
+ 
+    description: {
+      type: String,
+      required: true
+    },
+ 
+    duration: {
+      type: String,
+      required: true
+    },
+ 
+    level: {
+      type: String,
+      required: true
+    },
+ 
 
-const schema = new mongoose.Schema({
-  courseName: { type: String, required: true },
-  description:{ type: String, required: true },
-  duration:   { type: String, required: true },
-  level:      { type: String, required: true },
-
-  skills: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Skills",  
-    required: true
-  }]
-});
-
-export const course = mongoose.model("Course", schema);
+    skills: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Skills",
+        required: true
+      }
+    ]
+  },
+  { timestamps: true }
+);
+ 
+export const course = mongoose.model("Course", courseSchema);
+ 
+ 
---
+++ routes/batchRoutes.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { createBatch, getAllBatches, getBatchById, updateBatch } from '../controller/batchController.js';
+
+const batchroutes =express.Router()
+
+batchroutes.post('/createBatch',createBatch)
+batchroutes.get('/getAllBatch',getAllBatches)
+batchroutes.get('/getAllBatchById',getBatchById)
+batchroutes.patch('/updateBatch',updateBatch)
+batchroutes.delete('/deleteBatch',)
